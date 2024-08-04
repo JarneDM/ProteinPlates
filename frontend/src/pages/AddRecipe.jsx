d5eb26001e9c8d13beba42e07fb4bfae3d6f13a8
Jarne <jarne.demeyer05@hotmail.com>
2024-08-04 21:05:45 +0200

    adding html to the add recipe page

--- frontend/src/pages/AddRecipe.jsx
+++ frontend/src/pages/AddRecipe.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 function AddRecipe() {
   return (
-    <div>
-      Add a Recipe
+    <div className='container-ar'>
+      <div className="inputs">
+        <div className="input-name">
+          <input type="text" className='name' placeholder='Name of the recipe'/>
+        </div>
+        <div className="input-ing">
+          <input type="text" name="ingredient" id="ingredient" className='ingredient' placeholder='Ingredients...'/>
+        </div>
+        <div className="input-instr">
+          <input type="text" name="instructions" id="instructions" className='instructions' placeholder='Instructions...'/>
+        </div>
+        <div className="input-nutr">
+          <input className='nutr-inp' type="number" placeholder='Calories'/>
+          <input className='nutr-inp' type="number" placeholder='Fat'/>
+          <input className='nutr-inp' type="number" placeholder='Carbs'/>
+          <input className='nutr-inp' type="number" placeholder='Protein'/>
+        </div>
+      </div>
     </div>
   )
 }
