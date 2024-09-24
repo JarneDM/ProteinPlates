8d918fe0c3df99939831d0a615b6eb7119f1acde
Jarne <jarne.demeyer05@hotmail.com>
2024-09-24 18:06:38 +0200

    add button

--- frontend/src/pages/AddRecipe.jsx
+++ frontend/src/pages/AddRecipe.jsx
@@ -1,27 +1,47 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React from "react";
 
 function AddRecipe() {
   return (
-    <div className='container-ar'>
+    <div className="container-ar">
       <div className="inputs">
         <div className="input-name">
-          <input type="text" className='name' placeholder='Name of the recipe'/>
+          <input
+            type="text"
+            className="name"
+            placeholder="Name of the recipe"
+          />
         </div>
         <div className="input-ing">
-          <input type="text" name="ingredient" id="ingredient" className='ingredient' placeholder='Ingredients...'/>
+          <input
+            type="text"
+            name="ingredient"
+            id="ingredient"
+            className="ingredient"
+            placeholder="Ingredients..."
+          />
         </div>
         <div className="input-instr">
-          <input type="text" name="instructions" id="instructions" className='instructions' placeholder='Instructions...'/>
+          <input
+            type="text"
+            name="instructions"
+            id="instructions"
+            className="instructions"
+            placeholder="Instructions..."
+          />
         </div>
         <div className="input-nutr">
-          <input className='nutr-inp' type="number" placeholder='Calories'/>
-          <input className='nutr-inp' type="number" placeholder='Fat'/>
-          <input className='nutr-inp' type="number" placeholder='Carbs'/>
-          <input className='nutr-inp' type="number" placeholder='Protein'/>
+          <input className="nutr-inp" type="number" placeholder="Calories" />
+          <input className="nutr-inp" type="number" placeholder="Fat" />
+          <input className="nutr-inp" type="number" placeholder="Carbs" />
+          <input className="nutr-inp" type="number" placeholder="Protein" />
         </div>
+        <button type="submit" id="add-btn">
+          Add
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default AddRecipe
+export default AddRecipe;
